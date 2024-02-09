5e7bf4fd70037463fa9f7ecdfb3dc182b3660015
meeshh <mick.f4rr3ll@gmail.com>
2024-02-09 15:05:36 +0100

    logo

--- BeerApp - Senior/src/components/TopBar/index.tsx
+++ BeerApp - Senior/src/components/TopBar/index.tsx
@@ -1,48 +1,55 @@
-import { AppBar, IconButton, InputBase, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
-import { styled, alpha } from '@mui/material/styles';
+import { styled, alpha } from "@mui/material/styles";
 
 import { Search as SearchIcon } from "@mui/icons-material";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 interface Props {
@@ -53,7 +60,6 @@ interface Props {
 const TopBar = (props: Props) => {
   return (
     <AppBar
-      // position="fixed"
       sx={{
         width: { sm: `calc(100% - ${props.drawerWidth}px)` },
         ml: { sm: `${props.drawerWidth}px` },
@@ -69,9 +75,14 @@ const TopBar = (props: Props) => {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
-          Logo
-        </Typography>
+        <Avatar
+          variant="rounded"
+          sx={{ width: 32, height: 32, mr: {
+            xs: 2,
+            sm: 0,
+          } }}
+          src="/icons/favicon-32.png"
+        />
         <Search>
           <SearchIconWrapper>
             <SearchIcon />
